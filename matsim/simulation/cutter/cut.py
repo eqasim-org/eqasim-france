968ebae89d6d85edd3886b67f3f40204dcceb918
Valentin LE BESCOND <valentin.lebescond@univ-eiffel.fr>
2025-05-15 23:36:17 +0200

    permit cutter to run before the full simulation

--- matsim/simulation/cutter/cut.py
+++ matsim/simulation/cutter/cut.py
@@ -1,6 +1,6 @@
 import os
 import matsim.runtime.eqasim as eqasim
-
+import geopandas as gpd
 from synpp import ConfigurationContext, ExecuteContext
 
 def configure(context: ConfigurationContext):
@@ -8,23 +8,30 @@ def configure(context: ConfigurationContext):
     context.config("output_prefix", "ile_de_france_")
 
     context.config("cutter_name", "cutter")
+    context.config("cutter_after_full_simulation", True)
 
     context.stage("data.cutter.geometry")
     context.stage("matsim.runtime.java")
     context.stage("matsim.runtime.eqasim")
 
-    context.stage("matsim.simulation.full_run")
+    context.stage("matsim.output")
+
+    if context.config("cutter_after_full_simulation"):
+        context.stage("matsim.simulation.full_run")
+
 
 
 def execute(context: ExecuteContext):
+    output_path = context.config("output_path")
+
     config_path = "%s/%sconfig.xml" % (
-        context.config("output_path"),
+        output_path,
         context.config("output_prefix"),
     )
 
     cutter_name = context.config("cutter_name")
 
-    gpd_cutter = context.stage("data.cutter.geometry")
+    gpd_cutter: gpd.GeoDataFrame = context.stage("data.cutter.geometry")
     
     geometry_file = "%s/%s.shp" % (context.path(), cutter_name)
     
@@ -32,22 +39,27 @@ def execute(context: ExecuteContext):
         geometry_file
     )
 
-    initial_simulation_path = "%s/simulation_output" % context.config("output_path")
     cutter_simulation_path = "%s/%s" % (
-        context.config("output_path"),
+        output_path,
         cutter_name,
     )
+    args = [
+        "--config-path", config_path,
+        "--output-path", cutter_simulation_path,
+        "--extent-path", geometry_file,
+        "--prefix", cutter_name + "_"
+    ]
+
+    if context.config("cutter_after_full_simulation"):
+        args += [
+            "--config:plans.inputPlansFile", "%s/simulation_output/output_plans.xml.gz" % output_path,
+        ]
+    
     eqasim.run(
         context,
         "org.eqasim.core.scenario.cutter.RunScenarioCutter",
-        [
-            "--config-path", config_path,
-            "--output-path", cutter_simulation_path,
-            "--extent-path", geometry_file,
-            "--config:plans.inputPlansFile", "%s/output_plans.xml.gz" % initial_simulation_path,
-            "--prefix", cutter_name + "_"
-        ],
-        cwd=initial_simulation_path,
+        args,
+        cwd=output_path,
     )
 
     assert os.path.exists(
