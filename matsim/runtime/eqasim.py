5252eb366e76a780e3b3af0a78b2166c4e287f41
Sebastian Hörl <hoerl.sebastian@gmail.com>
2025-07-23 15:47:06 +0200

    update

--- matsim/runtime/eqasim.py
+++ matsim/runtime/eqasim.py
@@ -7,7 +7,7 @@ import matsim.runtime.maven as maven
 
 DEFAULT_EQASIM_VERSION = "2.0.0"
 DEFAULT_EQASIM_BRANCH = "feat/model-2024"
-DEFAULT_EQASIM_COMMIT = "7630f9a"
+DEFAULT_EQASIM_COMMIT = "5d8815d"
 
 def configure(context):
     context.stage("matsim.runtime.git")
