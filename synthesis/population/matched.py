a7a740eb5dd28ae63f86de7051162cd05180b4d5
Sebastian Hörl <hoerl.sebastian@gmail.com>
2025-12-18 13:42:29 +0100

    revert potential fix and test on both windows

--- synthesis/population/matched.py
+++ synthesis/population/matched.py
@@ -53,7 +53,7 @@ def statistical_matching(progress, df_source, source_identifier, weight, df_targ
     df_target = df_target[[target_identifier] + columns].copy()
 
     # Sort data frames
-    df_source = df_source.sort_values(by = columns + [source_identifier, weight])
+    df_source = df_source.sort_values(by = columns)
     df_target = df_target.sort_values(by = columns)
 
     # Find unique values for all columns
