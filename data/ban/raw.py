7245b2e89e3107aa28813672c5202959031d3de7
ychiche <yoann.chiche@minesparis.psl.eu>
2026-07-23 10:19:46 +0200

    feat(ban): code insee not found in the ban are skipped.

--- data/ban/raw.py
+++ data/ban/raw.py
@@ -1,7 +1,6 @@
 import os, glob
 import pandas as pd
 import geopandas as gpd
-import numpy as np
 
 """
 This stage loads the raw data from the new French address registry (BAN).
@@ -30,7 +29,7 @@ def execute(context):
     for source_path in find_ban("{}/{}".format(context.config("data_path"), context.config("ban_path"))):
         print("Reading {} ...".format(source_path))
 
-        df_partial = pd.read_csv(source_path, 
+        df_partial = pd.read_csv(source_path,
             compression = "gzip", sep = ";", usecols = BAN_DTYPES.keys(), dtype = BAN_DTYPES)
         
         # Filter by commune.
@@ -41,13 +40,29 @@ def execute(context):
         if len(df_partial) > 0:
             df_ban.append(df_partial)
     
-    df_ban = pd.concat(df_ban)
+    if len(df_ban) > 0:
+        df_ban = pd.concat(df_ban, ignore_index = True)
+    else:
+        df_ban = pd.DataFrame(columns = ["commune_id", "x", "y"])
+
     df_ban = gpd.GeoDataFrame(
         df_ban, geometry = gpd.points_from_xy(df_ban.x, df_ban.y), crs = "EPSG:2154")
     
-    # Check that we cover all requested communes at least once
-    for commune_id in requested_communes:
-        assert np.count_nonzero(df_ban["commune_id"] == commune_id) > 0
+    # Check that we cover all requested communes at least once.
+    # Report the corresponding IRIS and department to make missing BAN
+    # coverage easier to diagnose than a bare AssertionError.
+    available_communes = set(df_ban["commune_id"].astype(str).unique())
+    requested_communes = set(map(str, requested_communes))
+    missing_communes = requested_communes - available_communes
+
+    if missing_communes:
+        missing_codes = df_codes[
+            df_codes["commune_id"].astype(str).isin(missing_communes)
+        ][["iris_id", "commune_id", "departement_id"]].drop_duplicates()
+
+        print("Warning: communes missing in BAN; continuing with available addresses:")
+        print(missing_codes.sort_values("commune_id").to_string(index=False))
+
 
     return df_ban[["geometry"]]
 
